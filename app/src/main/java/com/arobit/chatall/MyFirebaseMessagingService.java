16969120e3f7436c373848cd82bcf5c69b97e019
root <soumojyotiganguly270@gmail.com>
2020-09-02 13:09:09 +0530

    fix notification

--- app/src/main/java/com/arobit/chatall/MyFirebaseMessagingService.java
+++ app/src/main/java/com/arobit/chatall/MyFirebaseMessagingService.java
@@ -1,8 +1,10 @@
 package com.arobit.chatall;
 
+import android.app.ActivityManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 
@@ -12,10 +14,10 @@ import androidx.core.app.NotificationCompat;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.List;
 import java.util.Map;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
-
 
 
     @Override
@@ -25,46 +27,65 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         String title = remoteMessage.getNotification().getTitle();
         String body = remoteMessage.getNotification().getBody();
 
-        Map<String, String> extraData = remoteMessage.getData();
-
-
-
-
-        String brandId = extraData.get("brandId");
-        String category = extraData.get("category");
-
-        NotificationCompat.Builder notificationBuilder =
-                new NotificationCompat.Builder(this, "TAC")
-                        .setContentTitle(title)
-                        .setContentText(body)
-                        .setSmallIcon(R.drawable.ic_launcher_background);
-
-        Intent intent;
-        if (category.equals("shoes")) {
-            intent = new Intent(this, GroupsActivity.class);
-
+        if (isForeground(getApplicationContext())) {
+            //if in forground then your operation
+            // if app is running them
         } else {
-            intent = new Intent(this, GroupsActivity.class);
+
+
+            Map<String, String> extraData = remoteMessage.getData();
+
+
+            String brandId = extraData.get("brandId");
+            String category = extraData.get("category");
+
+            NotificationCompat.Builder notificationBuilder =
+                    new NotificationCompat.Builder(this, "TAC")
+                            .setContentTitle(title)
+                            .setContentText(body)
+                            .setSmallIcon(R.drawable.ic_launcher_background);
+
+            Intent intent;
+            if (category.equals("shoes")) {
+                intent = new Intent(this, GroupsActivity.class);
+
+            } else {
+                intent = new Intent(this, GroupsActivity.class);
+
+            }
+            //intent.putExtra("brandId", brandId);
+            //intent.putExtra("category", category);
+
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 10, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            notificationBuilder.setContentIntent(pendingIntent);
+
+
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+
+            int id = (int) System.currentTimeMillis();
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                NotificationChannel channel = new NotificationChannel("TAC", "demo", NotificationManager.IMPORTANCE_HIGH);
+                notificationManager.createNotificationChannel(channel);
+            }
+            notificationManager.notify(id, notificationBuilder.build());
 
         }
-        //intent.putExtra("brandId", brandId);
-        //intent.putExtra("category", category);
 
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 10, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        notificationBuilder.setContentIntent(pendingIntent);
+    }
 
 
-        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-
-
-        int id =  (int) System.currentTimeMillis();
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            NotificationChannel channel = new NotificationChannel("TAC","demo", NotificationManager.IMPORTANCE_HIGH);
-            notificationManager.createNotificationChannel(channel);
+    private static boolean isForeground(Context context) {
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> tasks = am.getRunningAppProcesses();
+        final String packageName = context.getPackageName();
+        for (ActivityManager.RunningAppProcessInfo appProcess : tasks) {
+            if (ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND == appProcess.importance && packageName.equals(appProcess.processName)) {
+                return true;
+            }
         }
-        notificationManager.notify(id,notificationBuilder.build());
-
+        return false;
     }
 }
